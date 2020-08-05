aea358d24013c4e82f1b2283a31d3d935c0cb5f5
Ishaanis13 <69059812+Ishaanis13@users.noreply.github.com>
2020-08-05 18:15:43 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var rect1,rect2;
+
+
+function setup() {
+  createCanvas(800,400);
+rect1 = createSprite(200,200,40,20);
+rect2 = createSprite(400,200,20,40);
+rect1.shapeColor = "green";
+rect2.shapeColor = "green";
+
+}
+
+
+
+
+function draw() {
+  background(255,255,255); 
+  rect2.x= World.mouseX;
+  rect2.y=World.mouseY;
+  if(rect2.x-rect1.x<rect1.width/2+rect2.width/2&&rect1.x-rect2.x<rect1.width/2+rect2.width/2&&
+    rect2.y-rect1.y<rect1.height/2+rect2.height/2&&rect1.y-rect2.y<rect2.height/2+rect1.height/2){
+rect1.shapeColor="red";
+rect2.shapeColor = "red";
+  }
+else{
+rect1.shapeColor ="green";
+rect2.shapeColor ="green";
+}
+  drawSprites();
+}
